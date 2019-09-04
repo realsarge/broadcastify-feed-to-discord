335a4db2917a0e94a07fd944b35fc2f97aa6e95f
Ivan <ivanzhestkov99@gmail.com>
2019-09-04 22:07:55 +0300

    More customisation and stability for everyone! ✨

--- scanner.js
+++ scanner.js
@@ -12,15 +12,18 @@ const {
 } = require('discord.js');
 var request = require("request");
 var cheerio = require('cheerio');
-var phantom = require('phantom');
+
+var puppeteer = require('puppeteer');
 
 // SET CHANNELS
-var scannerchannel = "CHANNELID"
-var textchannel = "CHANNELID"
+var scannerchannel = "SCANNER"
+var textchannel = "LOG"
 
 // SET VARIABLES
 var currentstream = defaultstream
 var changeto
+
+var top = 2 // # OF FEED FROM TOP OF THE PAGE
 
 // START
 client.on('ready', () => {
@@ -28,55 +31,59 @@ client.on('ready', () => {
     let channel = client.channels.get(scannerchannel);
     client.user.setActivity(currentstream.name, {
         type: "LISTENING",
-        url: currentstream.stream
+        url: currentstream.name
     })
-    console.log('BOT Started.');
+    console.log('Init.');
     checktop();
     setInterval(checktop, 1200000); // CHECK FREQUENCY
 });
 
 // GET TOP STREAM
 function checktop() {
+    console.log('Check Started.')
     request({
         uri: "https://www.broadcastify.com/listen/top",
     }, function (error, response, html) {
         var C$ = cheerio.load(html);
-        topstreamname = (C$('.w100 a').first().text()); // GET STREAM NAME
-        topstreamlink = 'https://www.broadcastify.com' + (C$('.w100 a').attr('href')) + '/web'; // GET STREAM LINK
-        phantomcheck(topstreamname, topstreamlink)
-        console.log('Top stream: ' + topstreamname)
+        topstreamname = (C$('.w100 a').eq(top+2).text()); // GET STREAM NAME
+        topstreamlink = 'https://www.broadcastify.com' + (C$('.w100 a').eq(top+2).attr('href')) + '/web'; // GET STREAM LINK
+        console.log(topstreamname)
+        console.log("Current stream: " + currentstream.name)
+        if ((topstreamname) && (topstreamlink)) {
+            console.log("Got TOP stream name and link.")
+            console.log("TOP stream: " + topstreamname)
+            if (topstreamname !== currentstream.name) {
+                console.log("New TOP stream found, updating.")
+                browsercheck(topstreamname, topstreamlink)
+            }
+        }
     })
 };
 
-function phantomcheck(topstreamname, topstreamlink) {
-    if ((topstreamname) && (topstreamlink)) {
-        phantom.create().then(function (ph) {
-            ph.createPage().then(function (page) {
-                page.open(topstreamlink).then(function (status) {
-                    var p = page.evaluate(function () {
-                        return document.getElementsByTagName('audio')[0].src // FIND AUDIO ELEMENT
-                    });
-                    p.then(function (pagecontent) {
-                        if (pagecontent[0] !== undefined) {
-                            if (pagecontent !== currentstream.stream) {
-                                update(pagecontent, topstreamname);
-                                console.log('Update: ' + pagecontent)
-                            } else {
-                                console.log('Check: ' + pagecontent)
-                            }
-                        } else {
-                            console.log(pagecontent)
-                        }
-                        ph.exit();
-                    });
-                });
-            });
+function browsercheck(topstreamname, topstreamlink) {
+    console.log("Browser check started.");
+    (async () => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
-    }
+        const page = await browser.newPage();
+        await page.goto(topstreamlink);
+        console.log("URL opened.")
+        var getlink = await page.evaluate(() => {
+            var audioelement = document.getElementsByTagName('audio')[0].src;
+            console.log("Audio element found, direct stream link: " + audioelement)
+            return audioelement;
+        });
+        update(getlink, topstreamname);
+        await browser.close();
+        console.log("Browser closed.")
+    })();
 };
 
 // UPDATE CURRENT STREAM
 function update(streamlink, streamname) {
+    console.log('Updating stream.')
     let channel = client.channels.get(scannerchannel);
     changeto = {
         name: streamname,
@@ -95,5 +102,19 @@ function update(streamlink, streamname) {
             })
         })
 }
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        if (message.member.roles.find("name", "ROLE")) {
+            console.log('Got ping command.')
+            let channel = client.channels.get(scannerchannel);
+            message.channel.send("pong");
+            currentstream = ""
+            channel.leave()
+            channel.join()
+            checktop();
+        }
+    }
+});
 
 client.login("TOKEN");
